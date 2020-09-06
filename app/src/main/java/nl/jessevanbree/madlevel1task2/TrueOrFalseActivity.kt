d93115844938e03a75bcbc3cec0967e77833ec54
JessevanBree <jessevanbree@live.nl>
2020-09-06 21:00:31 +0200

    added input handling and checking

--- app/src/main/java/nl/jessevanbree/madlevel1task2/TrueOrFalseActivity.kt
+++ app/src/main/java/nl/jessevanbree/madlevel1task2/TrueOrFalseActivity.kt
@@ -2,22 +2,31 @@ package nl.jessevanbree.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import nl.jessevanbree.madlevel1task2.databinding.ActivityTrueOrFalseBinding
 
 class TrueOrFalseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrueOrFalseBinding
-    private var inputFF: String = "test"
+    private var inputTT: String = ""
+    private var inputTF: String = ""
+    private var inputFT: String = ""
+    private var inputFF: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTrueOrFalseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
     }
 
     fun initViews() {
         binding.submit.setOnClickListener { onSubmit() }
-        binding.inputFF.textWa
+        binding.inputTT.addTextChangedListener(textWatcher)
+        binding.inputTF.addTextChangedListener(textWatcher)
+        binding.inputFT.addTextChangedListener(textWatcher)
+        binding.inputFF.addTextChangedListener(textWatcher)
     }
 
     fun onSubmit() {
@@ -26,6 +35,27 @@ class TrueOrFalseActivity : AppCompatActivity() {
     }
 
     fun checkAnswers(): Boolean {
-        return false
+        return inputTT == "T" && inputTF == "F" && inputFT == "F" && inputFF == "F"
+    }
+
+    private val textWatcher = object: TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            when (editable) {
+                binding.inputTT.editableText -> {
+                    inputTT = editable.toString()
+                }
+                binding.inputTF.editableText -> {
+                    inputTF = editable.toString()
+                }
+                binding.inputFT.editableText -> {
+                    inputFT = editable.toString()
+                }
+                binding.inputFF.editableText -> {
+                    inputFF = editable.toString()
+                }
+            }
+        }
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 }
